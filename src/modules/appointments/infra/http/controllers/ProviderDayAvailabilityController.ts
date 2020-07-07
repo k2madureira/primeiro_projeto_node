e06996f71eadff274d68a586e2307295f9129e55
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-07-07 20:34:50 -0300

    routes / controller . appointments availability

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -5,7 +5,8 @@ import ListProviderDaysAvailabilityService from '@modules/appointments/services/
 
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
-    const { provider_id, day, month, year } = request.body;
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
 
     const listProviderDaysAvailabilityService = container.resolve(
       ListProviderDaysAvailabilityService,
