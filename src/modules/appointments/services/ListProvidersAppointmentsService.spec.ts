f2762ad2355321421351d7e122269f2c0023120d
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-07-08 18:59:42 -0300

    Feature: ListProviderAppointments / TDD

---
+++ src/modules/appointments/services/ListProvidersAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specifc day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointmentsService.execute({
+      provider_id: 'provider_id',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
