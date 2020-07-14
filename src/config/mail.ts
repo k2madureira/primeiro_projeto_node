fd9f257c754dc9c73f6b4a33bfed08ae59828c88
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-07-14 20:29:45 -0300

    Amazon SES

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,7 +1,20 @@
 interface IMailConfig {
   driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
 }
 
 export default {
   driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'dev@lpmadureira.me',
+      name: 'GoBarber',
+    },
+  },
 } as IMailConfig;
